3c3ae88415ae1fa9fc5b314c183b26c4deefda25
harshlocked7 <harshlocked7@gmail.com>
2023-04-14 21:18:59 +0530

    SellScrap Page 4 design final edits done.

--- src/pages/sellScrap4.js
+++ src/pages/sellScrap4.js
@@ -1,79 +1,101 @@
 import React from 'react';
-import { Box, Flex, Text, Stack, Divider, useColorModeValue } from '@chakra-ui/react';
+import { Box, Flex, Text, Stack, Divider, useColorModeValue, Progress, Heading, Button } from '@chakra-ui/react';
 import { useLocation } from 'react-router-dom';
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
 
 const Page4 = () => {
   const location = useLocation();
   const { selectedItems, address, latitude, longitude, pickupDate } = location.state || {};
 
   return (
-    <Box p={4} minH="calc(100vh - 64px)">
-      <Box mb={4}>
-        <Text fontSize="xl" fontWeight="bold">
-          Order Summary
-        </Text>
-      </Box>
-      <Stack spacing={4}>
-        <Box>
-          <Text fontSize="lg" fontWeight="bold">
-            Scrap Items:
-          </Text>
-          <Divider mt={2} mb={4} />
-          {selectedItems ? (
-            <Stack spacing={2}>
-              {selectedItems.map((item) => (
-                <Box key={item.id}>
-                  <Flex justify="space-between">
-                    <Text>{item.name}</Text>
-                    <Text>{item.price}</Text>
-                  </Flex>
-                  <Divider my={2} />
-                </Box>
-              ))}
-            </Stack>
-          ) : (
-            <Text>No scrap items selected</Text>
-          )}
-        </Box>
-        <Box>
-          <Text fontSize="lg" fontWeight="bold">
-            Home Address:
-          </Text>
-          <Divider mt={2} mb={4} />
-          {address ? (
-            <>
-              <Text mb={2}>{address}</Text>
-              <Box
-                w="100%"
-                h="200px"
-                bgColor={("gray.100", "gray.800")}
-                borderRadius={10}
-                overflow="hidden"
-              >
-                <iframe
-                  title="Map"
-                  width="100%"
-                  height="100%"
-                  frameBorder="0"
-                  style={{ border: 0 }}
-                  src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&q=${latitude},${longitude}`}
-                  allowFullScreen
-                />
+    <>
+    <Flex direction="column" align="center"></Flex><Navbar />
+    <Box w="70%" h="70px" bg="#38A327" borderRadius={15} p={1} mb={4} ml={200} >
+              {/* <Text fontSize="xl" fontWeight="semibold">Progress Tracker</Text> */}
+              <Flex justify="space-between" mt={2}>
+                  <Box flex="1">
+                      <Text fontSize="xl" fontWeight="bold">
+                          Select Scrap
+                      </Text>
+                  </Box>
+                  <Box flex="1">
+                      <Text fontSize="xl" fontWeight="bold">
+                          PickUp Date
+                      </Text>
+                  </Box>
+                  <Box flex="1">
+                      <Text fontSize="xl" fontWeight="bold">
+                          Confirm
+                      </Text>
+                  </Box>
+              </Flex>
+          </Box>
+          <Progress value={100} size="md" borderRadius={10} colorScheme="green" w="70%" ml={200} mb={5}/>
+          <Heading mr={600} fontSize={40} pb={5}>Pickup Confirmation</Heading>
+    <Box p={4} minH="calc(100vh - 200px)"  bg={'lightgrey'} borderRadius={10} w="70%" ml={200}>
+          <Box mb={4} bg={'lightgreen'} borderRadius={15}>
+              <Text fontSize="xl" fontWeight="bold" >
+                  Order Summary
+              </Text>
+          </Box>
+          <Stack spacing={4}>
+              <Box bg={'lightgreen'} borderRadius={15}>
+                  <Text fontSize="lg" fontWeight="bold" mr={700}>
+                      Scrap Items:
+                  </Text>
+                  <Divider mt={2} mb={4} />
+                  {selectedItems ? (
+                      <Stack spacing={2}>
+                          {selectedItems.map((item) => (
+                              <Box key={item.id}>
+                                  <Flex justify="space-between">
+                                      <Text>{item.name}</Text>
+                                      <Text>{item.price}</Text>
+                                  </Flex>
+                                  <Divider my={2} />
+                              </Box>
+                          ))}
+                      </Stack>
+                  ) : (
+                      <Text mr={600} pb={3}>No scrap items selected</Text>
+                  )}
               </Box>
-            </>
-          ) : (
-            <Text>No home address provided</Text>
-          )}
-        </Box>
-        <Box>
-          <Text fontSize="lg" fontWeight="bold">
-            Pick-Up Date:
-          </Text>
-          <Divider mt={2} mb={4} />
-          <Text>{pickupDate || 'No date selected'}</Text>
-        </Box>
-      </Stack>
-    </Box>
+              <Box bg={'lightgreen'} borderRadius={15}>
+                  <Text fontSize="lg" fontWeight="bold" mr={680}>
+                      Home Address:
+                  </Text>
+                  <Divider mt={2} mb={4} />
+                  {address ? (
+                      <>
+                          <Text mb={2}>{address}</Text>
+                          <Box
+                              w="100%"
+                              h="200px"
+                              bgColor={("gray.100", "gray.800")}
+                              borderRadius={10}
+                              overflow="hidden"
+                          >
+                          </Box>
+                      </>
+                  ) : (
+                      <Text pb={4} mr={600}>No home address provided</Text>
+                  )}
+              </Box>
+              <Box bg={'lightgreen'} borderRadius={15}>
+                  <Text fontSize="lg" fontWeight="bold" mr={680}>
+                      Pick-Up Date:
+                  </Text>
+                  <Divider mt={2} mb={4} />
+                  <Text mr={680}>{pickupDate || 'No date selected'}</Text>
+              </Box>
+          </Stack>
+          <Button colorScheme="green" mt={5} width={300}>
+                  Continue
+              </Button>
+      </Box>
+      <Footer/>
+      </>
   );
 };
 
